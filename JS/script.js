2e93628723ede525ba365f9cc4096e9beb280a46
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-07-24 22:55:45 +0200

    Add function that shuffle the card after press the start button

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,51 @@
-var cardArr = document.querySelectorAll('.card img');
-console.log(cardArr);
-var arrLi = document.querySelectorAll('.card');
+var imglist = document.querySelectorAll('.game__cardTable .card img');
+
+var srcArr = getSrc(imglist);
+
+var listLi = document.querySelectorAll('.game__cardTable li');
+console.log(listLi);
 
 var startButton = document.getElementById('start');
-console.log(startButton);
 
 startButton.addEventListener('click', function () {
-    cardArr.forEach(function (element) {
-       element.classList.add("black")
+    let deck = shuffle(srcArr);
+
+    imglist.forEach(function (element, index) {
+        element.setAttribute("src", deck[index]);
     });
+
+     imglist.forEach(function (element) {
+        element.classList.add("black")
+     });
 });
 
-arrLi.forEach(function (element) {
+listLi.forEach(function (element) {
     element.addEventListener('click', function () {
-            var child = this.children[0];
+            var child = this.firstElementChild;
             console.log(child);
             child.classList.remove("black");
         });
 });
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+function getSrc(array) {
+    let arr = [];
+
+    array.forEach(function (element) {
+        arr.push(element.getAttribute("src"))
+    });
+
+    return arr;
+}
