5a7db38055010079d3c6ea906a58a82f60042e78
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-07-25 08:30:35 +0200

    Add checkCard function that check if card have the same img and then leave this open

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,50 @@
+// Array with all img
 var imglist = document.querySelectorAll('.game__cardTable .card img');
-
+// Create array of src from img
 var srcArr = getSrc(imglist);
-
+//List of all Li element
 var listLi = document.querySelectorAll('.game__cardTable li');
 console.log(listLi);
-
+// Start button
 var startButton = document.getElementById('start');
 
-startButton.addEventListener('click', function () {
-    let deck = shuffle(srcArr);
+var showCard = [];
 
+// Function that start game
+startButton.addEventListener('click', function () {
+    // get shuffle array of src to all img
+    let deck = shuffle(srcArr);
+    // change actual src to a new random one
     imglist.forEach(function (element, index) {
         element.setAttribute("src", deck[index]);
     });
-
-     imglist.forEach(function (element) {
+    // add new class to card that hide img
+    imglist.forEach(function (element) {
         element.classList.add("black")
-     });
+    });
 });
 
+// Remove class black and show card
 listLi.forEach(function (element) {
-    element.addEventListener('click', function () {
-            var child = this.firstElementChild;
-            console.log(child);
-            child.classList.remove("black");
+        element.addEventListener('click', function () {
+            if (showCard.length !== 2) {
+                var child = this.firstElementChild;
+                console.log(child);
+                child.classList.toggle("black");
+                showCard.push(child);
+
+                if (showCard.length === 2) {
+                    checkCard(showCard);
+                    setTimeout(function () {
+                        showCard = []
+                    }, 2500)
+                }
+            };
         });
+
 });
 
+// Function that shuffle elements in array
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -40,6 +58,7 @@ function shuffle(array) {
     return array;
 }
 
+// function that create array with only a src to img
 function getSrc(array) {
     let arr = [];
 
@@ -48,4 +67,19 @@ function getSrc(array) {
     });
 
     return arr;
+}
+
+// Function that check if the card is the same or not
+
+function checkCard(array) {
+    console.log("wszedłem do funkcji");
+    if (array[0].getAttribute('src') === array[1].getAttribute('src')) {
+        console.log("para");
+    } else {
+        console.log("brak pary");
+       setTimeout(function(){
+            array[0].classList.toggle('black');
+            array[1].classList.toggle('black');}, 2000);
+
+    }
 }
