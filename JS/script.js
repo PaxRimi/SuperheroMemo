6dc4d5ff87caf9a9fffe91819e313be46eddc713
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-07-29 13:59:38 +0200

    Add function that count time from the start of the game

--- JS/script.js
+++ JS/script.js
@@ -6,17 +6,16 @@ var srcArr = getSrc(imglist);
 var listLi = document.querySelectorAll('.game__cardTable li');
 // Start button
 var startButton = document.getElementById('start');
+//Counter that prevent clicking a car before start game
+var Start = 0;
 //Span that count players points
-
 var points = document.getElementById('score');
-
 //Span that count players moves
 var moves = document.getElementById('moves');
-
 // Array with showing card to check them
 var showCard = [];
 // Add counter that multiple points if you choose couple in a row
-var multiplePointsCounter = 0;
+var multiplePointsCounter = document.getElementById('pointsX');
 // Points counter
 var actualPoints = 0 ;
 // Add music player element
@@ -25,9 +24,24 @@ var song = document.getElementById('song');
 var mute = document.getElementById('mute');
 // Add play icon element
 var playMusic = document.getElementById('play');
+// Add timer from html
+var timer = document.getElementById("time");
+// Add min and sec
+var second = 0;
+var minute = 0;
+var interval;
 
 // Function that start game
 startButton.addEventListener('click', function () {
+    Start++;
+    moves.innerText = 0;
+    points.innerText = 0;
+    multiplePointsCounter.innerText = 1;
+    actualPoints = 0;
+    second = 0;
+    minute = 0;
+    clearInterval(interval);
+    startTimer();
     // get shuffle array of src to all img
     let deck = shuffle(srcArr);
     // change actual src to a new random one
@@ -43,7 +57,7 @@ startButton.addEventListener('click', function () {
 // Remove class black and show card
 listLi.forEach(function (element) {
         element.addEventListener('click', function () {
-            if (showCard.length !== 2) {
+            if (showCard.length !== 2 && Start > 0) {
                 if ( showCard.length === 1) {
                     if ( showCard[0].parentElement !== this ) {
                         var child = this.firstElementChild;
@@ -55,7 +69,7 @@ listLi.forEach(function (element) {
                             checkCard(showCard);
                             setTimeout(function () {
                                 showCard = []
-                            }, 2500)
+                            }, 1500)
                         }
                     }
                 } else {
@@ -101,15 +115,15 @@ function checkCard(array) {
     if (array[0].getAttribute('src') === array[1].getAttribute('src')) {
         console.log("para");
         moves.innerText++;
-        multiplePointsCounter++;
         countPoints();
+        multiplePointsCounter.innerText++;
     } else {
         console.log("brak pary");
        setTimeout(function(){
             array[0].classList.toggle('black');
-            array[1].classList.toggle('black');}, 2000);
+            array[1].classList.toggle('black');}, 1000);
             moves.innerText++;
-            multiplePointsCounter = 0;
+            multiplePointsCounter.innerText = 1;
 
     }
 }
@@ -118,7 +132,7 @@ function checkCard(array) {
 
 function countPoints() {
     var minPoints = 10;
-    actualPoints += minPoints * multiplePointsCounter;
+    actualPoints += minPoints * multiplePointsCounter.innerText;
 
     points.innerText = actualPoints;
 }
@@ -135,4 +149,22 @@ playMusic.addEventListener("click", function () {
     playMusic.classList.add('active');
     mute.classList.remove('stop');
     song.play();
-})
+});
+
+
+
+
+function startTimer(){
+   interval = setInterval(function(){
+        second++;
+        timer.innerHTML = minute+"mins "+second+"secs";
+        if(second == 60){
+            minute++;
+            second = 0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+        },1000);
+}
